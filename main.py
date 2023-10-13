b9f755999c3c401965bd727170c0e3c273df9fca
narmina <namira.lir@gmail.com>
2023-10-13 09:58:45 +0300

    Исправление ошибок

--- main.py
+++ main.py
@@ -8,12 +8,14 @@ if student:
     print(f"Студент {student['full_name']} \nЗнает {','.join(student['skills'])}")
 else:
     print("Нет такого студента")
+    quit()
 
 search_profession_by_title = input(f"Выберите специальность для оценки студента {student['full_name']}")
 
 profession = get_profession_by_title(search_profession_by_title)
-skills, lacks, fitness = check_fitness(student, profession).values()
+
 if profession:
+    skills, lacks, fitness = check_fitness(student, profession).values()
     print(f"Пригодность: {fitness}%\n{student['full_name']} знает {','.join(skills)}\n{student['full_name']} не знает {','.join(lacks)}")
 else:
     print("У нас нет такой специальности")
